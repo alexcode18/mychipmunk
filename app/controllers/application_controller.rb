0e5b149ef27fe3620894aeba269c504edcd690ce
alexcode18 <aschattner@gmail.com>
2015-02-28 22:03:17 -0500

    trying to get the sessions to work. going to create a new branch.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,56 +1,10 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
 
-<<<<<<< HEAD
-  helper_method :current_user
-
-  def index
-    render layout: 'application', text: ''
-  end
-
-  def require_user
-    return if current_user
- 
-    respond_to do |format|
-      format.html { redirect_to login_path }
-      format.all  { render :text => 'unauthorized', :status => :unauthorized }
-    end
-  end
- 
-  def current_user
-    return @current_user if @current_user
- 
-    if session[:user_id]
-      @current_user = User.find(session[:user_id])
-    elsif (header = request.headers['Authorization'].to_s.sub('Basic ','')) != ''
-      header = Base64.decode64(header).split(':')
-      username = header.shift
-      password = header.join(':')
-      @current_user = User.authenticate(username, password)
-    end
-  end
- 
-  def create_user_session(user)
-    session[:user_id] = user.id
-  end
- 
-  def destroy_user_session
-    session[:user_id] = nil
-=======
-  helper_method :authenticate, :current_user, :logged_in?
-
-  def index
-  	render layout: 'application', text: ''
-  end
+  helper_method :authenticate
 
   def authenticate
-  	redirect_to root_path unless session[:current_user_id]
-  end
-
-  def logged_in?
-  	:current_user_id != nil
->>>>>>> b1e38a9611a04a7bd0969368249747aa5c26e9f7
+    redirect_to login_path unless session[:current_user_id]
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,46 +1,43 @@
 class SessionsController < ApplicationController
 
-<<<<<<< HEAD
-	respond_to :html, :json
- 
   def create
-    @user = User.authenticate(params[:email], params[:password])
- 
-    if @user
-      create_user_session(@user)
-      respond_with @user, location: '/', notice: "Login succesful."
+    user = User.find_by(parent_email: params[:parent_email])
+    if user && user.authenticate(params[:password])
+      session[:current_user_id] = user.id
+      render json: user.to_json(:include => :bear)
     else
-      respond_to do |format|
-        format.html { render 'new' }
-        format.json { render json: {error: "Invalid email or password."} }
-      end
+      render json: {error: "We're beary sorry. We can't find that email or password."}
     end
   end
- 
+
   def destroy
-    destroy_user_session
-    redirect_to '/', notice: "Logged out."
+    session[:current_user_id] = nil
+    redirect to root_path
   end
-=======
-	def new
-	end
 
-	def create
-		user = User.find_by(parent_email: params[:parent_email])
-		if user && user.authenticate(params[:password])
-			session[:current_user_id] = user.id
-			render json: user.to_json(:include => :bears)
-		else
-			# [:error, :warning, :notice].each do |type|
-   #      return flash[type] unless flash[type].blank?
-   #    end
-		end
-	end
-
-	def destroy
-		session[:current_user_id] = nil
-		redirect to root_path
-	end
->>>>>>> b1e38a9611a04a7bd0969368249747aa5c26e9f7
+# <<<<<<< HEAD
+# 	respond_to :html, :json
+ 
+#   def create
+#     @user = User.authenticate(params[:email], params[:password])
+ 
+#     if @user
+#       create_user_session(@user)
+#       respond_with @user, location: '/', notice: "Login succesful."
+#     else
+#       respond_to do |format|
+#         format.html { render 'new' }
+#         format.json { render json: {error: "Invalid email or password."} }
+#       end
+#     end
+#   end
+ 
+#   def destroy
+#     destroy_user_session
+#     redirect_to '/', notice: "Logged out."
+#   end
+# =======
+# 	def new
+# 	end
 
 end
