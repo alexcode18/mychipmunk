9c7a39c1a26383814b1d02f1c9d66e1542b8e86a
alexcode18 <aschattner@gmail.com>
2015-04-06 10:35:47 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,4 @@ ActiveRecord::Schema.define(version: 20150303193624) do
     t.inet     "last_sign_in_ip"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
 end
