faa2154afa3681b9fd9942641aa083f84c5d0ab5
alexcode18 <aschattner@gmail.com>
2015-07-29 22:12:24 -0400

    removed GAController

--- app/controllers/ga_controller.rb
+++ app/controllers/ga_controller.rb
@@ -1,10 +1,6 @@
 class GAController < ApplicationController
 
 	def create
-		auth = {:username => "aschattner@gmail.com", :password => 'airbender'}
-		# ENV["GA_KEY"]
-		url = "https://profiles.generalassemb.ly/profiles/1073/steps/the_lead"
-		@search = HTTParty.get(url, :basic_auth => auth)
-		render json: @search
+		
 	end
 end
